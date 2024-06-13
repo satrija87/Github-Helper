bd45d9bfa65ab8fb99ac3232b372d57f6e2a26c3
Darya Miskevich <satrija87@gmail.com>
2024-06-13 17:52:28 +0200

    fix userPAge

--- src/components/UserCard.jsx
+++ src/components/UserCard.jsx
@@ -14,7 +14,9 @@ const UserCard = ({ user }) => {
       </div>
       <div>
         <p className="text-xl font-bold my-4 ">{user?.name}</p>
-        <p className="text-sky-600 font-bold">{user?.login}</p>
+        <Link to={"https://github.com/" + user.login} className="text-sky-600 font-bold">
+          {user?.login}
+        </Link>
       </div>
       <div className="my-8 flex  gap-4">
         <span className="flex ">
